f207c128ef90436bdd471e4f6cf4d830b105fc5c
Zhao KK <kikakkz@hotmail.com>
2023-08-23 09:25:33 +0000

    Add good

---
+++ api/good/query.go
@@ -0,0 +1,74 @@
+//nolint:dupl
+package good
+
+import (
+	"context"
+
+	good1 "github.com/NpoolPlatform/good-gateway/pkg/good"
+
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+
+	"github.com/NpoolPlatform/go-service-framework/pkg/logger"
+	npool "github.com/NpoolPlatform/message/npool/good/gw/v1/good"
+)
+
+func (s *Server) GetGood(ctx context.Context, in *npool.GetGoodRequest) (*npool.GetGoodResponse, error) {
+	handler, err := good1.NewHandler(
+		ctx,
+		good1.WithID(&in.ID, true),
+	)
+	if err != nil {
+		logger.Sugar().Errorw(
+			"GetGood",
+			"In", in,
+			"Error", err,
+		)
+		return &npool.GetGoodResponse{}, status.Error(codes.Aborted, err.Error())
+	}
+
+	info, err := handler.GetGood(ctx)
+	if err != nil {
+		logger.Sugar().Errorw(
+			"GetGood",
+			"In", in,
+			"Error", err,
+		)
+		return &npool.GetGoodResponse{}, status.Error(codes.Aborted, err.Error())
+	}
+
+	return &npool.GetGoodResponse{
+		Info: info,
+	}, nil
+}
+
+func (s *Server) GetGoods(ctx context.Context, in *npool.GetGoodsRequest) (*npool.GetGoodsResponse, error) {
+	handler, err := good1.NewHandler(
+		ctx,
+		good1.WithOffset(in.Offset),
+		good1.WithLimit(in.Limit),
+	)
+	if err != nil {
+		logger.Sugar().Errorw(
+			"GetGoods",
+			"In", in,
+			"Error", err,
+		)
+		return &npool.GetGoodsResponse{}, status.Error(codes.Aborted, err.Error())
+	}
+
+	infos, total, err := handler.GetGoods(ctx)
+	if err != nil {
+		logger.Sugar().Errorw(
+			"GetGoods",
+			"In", in,
+			"Error", err,
+		)
+		return &npool.GetGoodsResponse{}, status.Error(codes.Aborted, err.Error())
+	}
+
+	return &npool.GetGoodsResponse{
+		Infos: infos,
+		Total: total,
+	}, nil
+}
---
+++ api/good/update.go
@@ -0,0 +1,62 @@
+//nolint:dupl
+package good
+
+import (
+	"context"
+
+	good1 "github.com/NpoolPlatform/good-gateway/pkg/good"
+
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+
+	"github.com/NpoolPlatform/go-service-framework/pkg/logger"
+	npool "github.com/NpoolPlatform/message/npool/good/gw/v1/good"
+)
+
+func (s *Server) UpdateGood(ctx context.Context, in *npool.UpdateGoodRequest) (*npool.UpdateGoodResponse, error) {
+	handler, err := good1.NewHandler(
+		ctx,
+		good1.WithID(&in.ID, true),
+		good1.WithDeviceInfoID(in.DeviceInfoID, false),
+		good1.WithDurationDays(in.DurationDays, false),
+		good1.WithCoinTypeID(in.CoinTypeID, false),
+		good1.WithVendorLocationID(in.VendorLocationID, false),
+		good1.WithPrice(in.Price, false),
+		good1.WithBenefitType(in.BenefitType, false),
+		good1.WithGoodType(in.GoodType, false),
+		good1.WithTitle(in.Title, false),
+		good1.WithUnit(in.Unit, false),
+		good1.WithUnitAmount(in.UnitAmount, false),
+		good1.WithSupportCoinTypeIDs(in.SupportCoinTypeIDs, false),
+		good1.WithDeliveryAt(in.DeliveryAt, false),
+		good1.WithStartAt(in.StartAt, false),
+		good1.WithTestOnly(in.TestOnly, false),
+		good1.WithTotal(in.Total, false),
+		good1.WithPosters(in.Posters, false),
+		good1.WithLabels(in.Labels, false),
+		good1.WithBenefitIntervalHours(in.BenefitIntervalHours, false),
+		good1.WithUnitLockDeposit(in.UnitLockDeposit, false),
+	)
+	if err != nil {
+		logger.Sugar().Errorw(
+			"UpdateGood",
+			"In", in,
+			"Error", err,
+		)
+		return &npool.UpdateGoodResponse{}, status.Error(codes.Aborted, err.Error())
+	}
+
+	info, err := handler.UpdateGood(ctx)
+	if err != nil {
+		logger.Sugar().Errorw(
+			"UpdateGood",
+			"In", in,
+			"Error", err,
+		)
+		return &npool.UpdateGoodResponse{}, status.Error(codes.Aborted, err.Error())
+	}
+
+	return &npool.UpdateGoodResponse{
+		Info: info,
+	}, nil
+}
