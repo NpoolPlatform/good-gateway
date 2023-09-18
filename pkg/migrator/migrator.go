414477c58c5f42500a7ac08424f3c89e8caff317
Zhao KK <kikakkz@hotmail.com>
2023-09-18 04:15:02 +0000

    Return

--- pkg/migrator/migrator.go
+++ pkg/migrator/migrator.go
@@ -173,7 +173,6 @@ func migrateAppGoodStock(ctx context.Context, tx *ent.Tx) error {
 			Save(ctx); err != nil {
 			return err
 		}
-		return nil
 	}
 	return nil
 }
@@ -210,7 +209,6 @@ func migrateAppDefaultGood(ctx context.Context, tx *ent.Tx) error {
 			Save(ctx); err != nil {
 			return err
 		}
-		return nil
 	}
 	return nil
 }
