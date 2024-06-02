33e8f52ed7cec9d5abbbb06562642104b9e3795d
Zhao KK <kikakkz@hotmail.com>
2024-06-02 07:26:27 +0000

    Fix: http://47.116.78.131:10083/issues/3549

--- pkg/app/good/poster/delete.go
+++ pkg/app/good/poster/delete.go
@@ -3,6 +3,7 @@ package poster
 import (
 	"context"
 
+	wlog "github.com/NpoolPlatform/go-service-framework/pkg/wlog"
 	appgoodpostermwcli "github.com/NpoolPlatform/good-middleware/pkg/client/app/good/poster"
 	npool "github.com/NpoolPlatform/message/npool/good/gw/v1/app/good/poster"
 )
@@ -18,11 +19,19 @@ func (h *Handler) DeletePoster(ctx context.Context) (*npool.Poster, error) {
 		},
 	}
 	if err := handler.checkPoster(ctx); err != nil {
-		return nil, err
+		return nil, wlog.WrapError(err)
+	}
+
+	info, err := h.GetPoster(ctx)
+	if err != nil {
+		return nil, wlog.WrapError(err)
+	}
+	if info == nil {
+		return nil, wlog.Errorf("invalid poster")
 	}
 
 	if err := appgoodpostermwcli.DeletePoster(ctx, h.ID, h.EntID); err != nil {
-		return nil, err
+		return nil, wlog.WrapError(err)
 	}
-	return h.GetPoster(ctx)
+	return info, nil
 }
