69564062f272fb6348341e04705a3cf7fadb6b87
Zhao KK <kikakkz@hotmail.com>
2024-06-02 07:30:21 +0000

    Fix: http://47.116.78.131:10083/issues/3547

--- pkg/app/good/label/delete.go
+++ pkg/app/good/label/delete.go
@@ -3,6 +3,7 @@ package label
 import (
 	"context"
 
+	wlog "github.com/NpoolPlatform/go-service-framework/pkg/wlog"
 	appgoodlabelmwcli "github.com/NpoolPlatform/good-middleware/pkg/client/app/good/label"
 	npool "github.com/NpoolPlatform/message/npool/good/gw/v1/app/good/label"
 )
@@ -18,11 +19,19 @@ func (h *Handler) DeleteLabel(ctx context.Context) (*npool.Label, error) {
 		},
 	}
 	if err := handler.checkLabel(ctx); err != nil {
-		return nil, err
+		return nil, wlog.WrapError(err)
+	}
+
+	info, err := h.GetLabel(ctx)
+	if err != nil {
+		return nil, wlog.WrapError(err)
+	}
+	if info == nil {
+		return nil, wlog.Errorf("invalid label")
 	}
 
 	if err := appgoodlabelmwcli.DeleteLabel(ctx, h.ID, h.EntID); err != nil {
-		return nil, err
+		return nil, wlog.WrapError(err)
 	}
-	return h.GetLabel(ctx)
+	return info, nil
 }
